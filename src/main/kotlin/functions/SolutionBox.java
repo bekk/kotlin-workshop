ecaa4a11c7178fc4715574be5c4c8bc9ce4d3e0b
hmlund <hmlund@users.noreply.github.com>
2019-02-08 13:36:12 +0100

    Create SolutionBox.java

---
+++ src/main/kotlin/functions/SolutionBox.java
@@ -0,0 +1,18 @@
+package functions;
+
+public class SolutionBox {
+    int width;
+    int length;
+    int height;
+
+    public SolutionBox(int width, int length, int height){
+        this.width = width;
+        this.length = length;
+        this.height = height;
+    }
+    public SolutionBox( int height){
+        this.width = 10;
+        this.length = 10;
+        this.height = height;
+    }
+}
