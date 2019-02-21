658b45ec21815e05f75918c12d2fe3bf4f476103
hmlund <hmlund@users.noreply.github.com>
2019-02-21 16:37:52 +0100

    Added function tests and java implementation

--- src/main/kotlin/functions/Functions.kt
+++ src/main/kotlin/functions/Functions.kt
@@ -7,9 +7,13 @@ fun tests() {
 
     val boxOne = SolutionBox(99);
 
+    val SolutionBox = Solution.createBoxUsingWidthLengthHeight(10, 10,15);
+    val Box = createBox()
+
     utils.tests(
         { Solution.capitalizeName("hei") == capitalizeName("hei") },
         { Solution.capitalizeName("oLAV") == capitalizeName("oLAV") },
+        {SolutionBox.height == Box.height && SolutionBox.length == Box.lenght && SolutionBox.width == Box.width },
 
         { classEqualFieldNamesAndTypes(boxOne, createBox()) }
     )
@@ -45,7 +49,7 @@ fun capitalizeName(name: String): String = TODO()
  *
  */
 
-// TODO: implement createBox with three parameters (width,length and height). The width and length should have an default value of 10
+// TODO: implement createBox with three parameters (width,length and height). All parameters should have an default value of 10
 fun createBox(): Box = TODO()
 
 
