d03ab0a36fad78b8986a13c67022737bf32f8cef
Vegard Veiset <veiset@gmail.com>
2019-02-26 15:23:05 +0100

    Rename tasks in applyLetRun, add first task to test the kotlin environment

---
+++ src/main/kotlin/task01/testEnvironment/task01.kt
@@ -0,0 +1,20 @@
+package task01.testEnvironment
+
+import utils.tests
+
+/**
+ * Run the main method to check that everything is working correctly
+ *
+ * Expected result:
+ *   Test 1 - Success!
+ */
+fun main() {
+    tests(
+        { task01() }
+    )
+}
+
+/**
+ * This is where we typically will solve the tasks at hand.
+ */
+fun task01() = true
