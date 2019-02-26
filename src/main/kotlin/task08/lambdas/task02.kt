8d46053f2ed98b8c11e42c1fae2a63ceb76b7872
Vegard Veiset <veiset@gmail.com>
2019-02-26 18:16:41 +0100

    Update lambda task description

---
+++ src/main/kotlin/task08/lambdas/task02.kt
@@ -0,0 +1,21 @@
+package task08.lambdas
+
+import utils.tests
+
+fun main() {
+    tests("lambda with argument",
+        { 20 == task2() },
+        { "olleH!" == task3() }
+    )
+}
+
+fun numberMagic(lambda: (Int) -> Int) = lambda(10)
+
+/**
+ * If lambdas only have one argument in Kotlin we don't have to name it, and we can access it by the
+ * special reference `it`. So instead of writing `{ number -> number + 5}` we can simply write
+ * `{ it + 5 }`
+ *
+ * Lets pass a lambda to 'numberMagic' that doubles the number received.
+ */
+fun task2() = numberMagic { it * 2 }
