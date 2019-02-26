3e8c3fd072943f39c5e57422c38354cd00d48313
Vegard Veiset <veiset@gmail.com>
2019-02-26 16:14:10 +0100

    Refactor functional tasks

--- src/main/kotlin/functional/task03.kt
+++ src/main/kotlin/functional/task03.kt
@@ -1,7 +1,18 @@
 package functional
 
+import utils.tests
+
 fun main(args: Array<String>) {
-    test_task03()
+    val passengers = listOf(
+        passenger(yrjan, 22, 2200),
+        passenger(vetle, 15, 200),
+        passenger(petter, 44, 1600),
+        passenger(ole, 60, 1300)
+    )
+
+    tests("far passengers",
+        { passengers == task03(company) }
+    )
 }
 
 /**
