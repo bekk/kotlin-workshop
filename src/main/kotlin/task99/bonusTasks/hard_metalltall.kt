4887d166c3bc65055d7dc91a62ba7cc34803b217
Vegard Veiset <veiset@gmail.com>
2019-03-03 17:39:31 +0100

    Add more bonus tasks and labeled difficulty

---
+++ src/main/kotlin/task99/bonusTasks/hard_metalltall.kt
@@ -0,0 +1,37 @@
+/**
+B-gjengen er ute på tokt. De har, på utspekulert vis, klart å snike seg
+inn i pengebingen til Onkel Skrue, nærmere bestemt hvelvet for sjeldne metaller.
+Nå står de fremfor tre beholdere fylt med de uhyre sjeldne metallene foobarium,
+blingblingium og hjærn. Alle metallene er malt til finkornet støv.
+
+Men B-gjengen har et problem: Det er kun gjengmedlem 176-671 som har husket
+å ta med sekken sin, og den kan maksimalt romme 14 liter. Hvor mye av hvert
+metall bør de ta med seg av hvert metall? Heldigvis liker Onkel Skrue å ha
+oversikt over verdiene sine, så på veggen henger en skjerm med følgende data:
+
+   Foobarium: tetthet: 19,32 g/cm^3 - verdi: 337.56 kr/g - på lager: 65,1 kg
+   Blingblingium: tetthet: 10,49 g/cm^3 - verdi: 140.23 kr/g - på lager: 80,2 kg
+   Hjærn: tetthet: 5,96 g/cm^3 - verdi: 88.12 kr/g - på lager: 31,2 kg
+
+B-gjengen sliter med å regne ut dette selv, men har selvfølgelig forutsett
+at de ville komme til å støte på problemet. I forkant av innbruddet rekrutterte
+de det beste hacker-teamet Andeby har sett, nemlig dere! Og i forkant av
+brekket laget dere nemlig den kriminelt gode appen Skurkulatoren™️ som kan regne
+ut alle mulige tall og verdier som er av interesse for skurker.
+
+Kan dere gjengi koden dere skrev for B-gjengen som finner ut:
+
+Hvor mye av hvert metall de bør fylle sekken med?
+
+Hvor mange liter av hvert stoff får de med seg?
+
+Hva er den totale verdien?
+
+Hva veier sekken?
+
+*/
+
+
+fun main(args: Array<String>) {
+
+}
