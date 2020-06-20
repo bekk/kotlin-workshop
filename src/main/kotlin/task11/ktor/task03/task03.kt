9de10644c30f53e9450db301a571187b07d0ccda
Tia Firing <tia.firing@bekk.no>
2020-06-20 07:45:59 +0200

    client cleanup

--- src/main/kotlin/task11/ktor/task03/task03.kt
+++ src/main/kotlin/task11/ktor/task03/task03.kt
@@ -1,8 +1,16 @@
 package task11.ktor.task03
 
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.engine.apache.ApacheEngineConfig
+import io.ktor.client.features.json.JacksonSerializer
+import io.ktor.client.features.json.JsonFeature
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Location
 import io.ktor.locations.Locations
@@ -11,12 +19,47 @@ import io.ktor.response.respond
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import task11.ktor.task01.setupApplication
 
 /**
- * Lets look at some more use cases for Ktor. Ktor isn't only used for creating REST-interfaces you can also use it for calling other APIs.
- * In this task we have created an API for you that is supposed to return cocktail names from the CocktailDB API that matches the received cocktail
- * query parameter. We have started setting up the CocktailClient, and now your job is to finish it!
+ * Let's have a look at some more use cases for Ktor. Ktor isn't only used for creating REST-interfaces, you can also use it for calling other APIs.
+ * In this task we have created an API for you that takes a cocktail name as a query parameter and is supposed to return any matching cocktail names
+ * from the CocktailDB API. We have started setting up the CocktailClient, and now your job is to finish it!
  */
+
+class CocktailClient {
+    private val config: HttpClientConfig<ApacheEngineConfig>.() -> Unit = {
+        install(JsonFeature) {
+            serializer = JacksonSerializer {
+                registerKotlinModule()
+                configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            }
+        }
+    }
+    private val httpClient = HttpClient(Apache, config)
+    private val cocktailDbEndpoint = "https://www.thecocktaildb.com/api/json/v1/1/search.php"
+
+    /**
+     * TODO:
+     * Use the httpClient to send a GET request to the cocktailDbEndpoint. Send the input cocktailName as
+     * a query param named "s", for instance like this: https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+     * The response will be of type Drinks, and you must map this response to a list of Strings.
+     */
+
+    fun getCocktail(cocktailName: String): List<String>? {
+        TODO()
+    }
+}
+
+data class Drinks(
+    val drinks: List<Drink>?
+)
+
+data class Drink(
+    val idDrink: String,
+    val strDrink: String
+)
+
 
 @KtorExperimentalLocationsAPI
 fun main() {
@@ -26,6 +69,7 @@ fun main() {
     data class CocktailRequest(val cocktail: String)
 
     embeddedServer(Netty, 8089) {
+        setupApplication()
         install(Locations)
         routing {
             get<CocktailRequest> { cocktailRequest ->
