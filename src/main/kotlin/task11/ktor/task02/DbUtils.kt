f6392016b1d5cae69ae747254e27c854dcd06d8f
Nicklas Utgaard <nutgaard@gmail.com>
2020-06-22 12:15:17 +0200

    Added margarita-cocktail

--- src/main/kotlin/task11/ktor/task02/DbUtils.kt
+++ src/main/kotlin/task11/ktor/task02/DbUtils.kt
@@ -16,6 +16,12 @@ fun createDbTables(dataSource: DataSource) {
     val sqlsetup: String = """
     CREATE TABLE cocktail(id VARCHAR(36) NOT NULL, name VARCHAR(100) NOT NULL);
     CREATE TABLE ingredient(id VARCHAR(36) NOT NULL, cocktail_id VARCHAR(36) NOT NULL, name VARCHAR(100) NOT NULL);
+    
+    INSERT INTO cocktail VALUES ('9b59090e-9968-4e43-9675-859f6cc64fda', 'Margarita');
+    INSERT INTO ingredient VALUES ('b2f0bdd0-8e54-45d7-896b-8f4f6f1aad3b', '9b59090e-9968-4e43-9675-859f6cc64fda', 'Tequila');
+    INSERT INTO ingredient VALUES ('8cf30024-26a6-4988-8c4e-68939d27515a', '9b59090e-9968-4e43-9675-859f6cc64fda', 'Triple sec');
+    INSERT INTO ingredient VALUES ('e871a63e-5fa1-42fb-8cdd-aa49ddbf0656', '9b59090e-9968-4e43-9675-859f6cc64fda', 'Lime juice');
+    INSERT INTO ingredient VALUES ('9bde8bdd-c473-4f76-9499-0bede908ce05', '9b59090e-9968-4e43-9675-859f6cc64fda', 'Salt');
     """
 
     dataSource
