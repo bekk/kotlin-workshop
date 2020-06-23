f20cd6d526897ebf3ac131dbd7d9f9d12387ae36
Jonas Triki <jonas.triki@bekk.no>
2020-06-23 11:13:05 +0200

    Fix typo in docs and test

--- src/main/kotlin/task08/lambdas/task03.kt
+++ src/main/kotlin/task08/lambdas/task03.kt
@@ -4,7 +4,7 @@ import utils.tests
 
 fun main() {
     tests("higher order",
-        { "olleH!" == task3() }
+        { "!olleH" == task3() }
     )
 }
 
@@ -14,7 +14,7 @@ fun stringFormat(formatter: (String) -> String): (String) -> String = { str -> f
  * Functions in Kotlin can also return functions!
  *
  * Lets create a string formatter that reverses a string and adds an exclamation mark (!)
- * After we have created our new formatter function, pass the string 'Hello' to it and
+ * After we have created our new formatter function, pass the string 'Hello!' to it and
  * return the result.
  */
 fun task3(): String {
